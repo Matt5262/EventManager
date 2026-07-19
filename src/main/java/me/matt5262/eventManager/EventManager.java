9d3be4ab7872fefe6c95384dcda366190d41046d
Matti <mattibruun2000@gmail.com>
2026-07-19 18:33:40 +0200

    Removed ProtocolLib again

--- src/main/java/me/matt5262/eventManager/EventManager.java
+++ src/main/java/me/matt5262/eventManager/EventManager.java
@@ -1,12 +1,5 @@
 package me.matt5262.eventManager;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.events.PacketEvent;
 import me.matt5262.eventManager.commands.SpawnCommand;
 import me.matt5262.eventManager.listeners.GuiListener;
 import org.bukkit.plugin.java.JavaPlugin;
